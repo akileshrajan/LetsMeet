99d215b672313a953ae98b4c45562c845cdbdad5
Ashay Rajimwale <ashay.rajimwale@gmail.com>
2015-11-24 21:27:04 -0600

    Schedule

--- app/src/main/java/com/utase1/letsmeet/activity/FreeTimeSchedulerInitiator.java
+++ app/src/main/java/com/utase1/letsmeet/activity/FreeTimeSchedulerInitiator.java
@@ -1,6 +1,7 @@
 package com.utase1.letsmeet.activity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -40,9 +41,12 @@ public class FreeTimeSchedulerInitiator  extends ArrayAdapter {
     private List<TimeParticipantMap> scheduleArrayList;
     String meetingName;
     String meetingDate;
+
     String meetingLocation;
     private static final String TAG = "FreeTimeSchedulerInitiator";
     private LayoutInflater inflater;
+    private ProgressDialog pDialog;
+    StringBuilder users=new StringBuilder();
 
     public FreeTimeSchedulerInitiator(Context context,int resource ,List<TimeParticipantMap> results,String meetingName,String meetingDate,String meetingLocation) {
         super(context, resource, results);
@@ -61,6 +65,8 @@ public class FreeTimeSchedulerInitiator  extends ArrayAdapter {
 
         final ViewHolder holder;
         notifyDataSetChanged();
+        pDialog = new ProgressDialog(getContext());
+        pDialog.setCancelable(false);
 
         if (convertView == null){
             convertView = inflater.inflate(R.layout.freetimeschedulerows, null);
@@ -79,41 +85,10 @@ public class FreeTimeSchedulerInitiator  extends ArrayAdapter {
             holder.btnSch.setOnClickListener(new View.OnClickListener() {
 
                 public void onClick(View view) {
-                    StringBuilder message;
-                    try
-                    {
-                        message = new StringBuilder();
-                        message.append("FreeTimeScheduler;");
-                        message.append(holder.txtMeetName.toString() + ";");
-                        message.append(holder.txtDate + ";");
-                        message.append(holder.txtTime.toString() + ";");
-                        if (!holder.txtLocation.toString().isEmpty()) {
-                            message.append(holder.txtLocation.toString());
-                        } else {
-                            message.append("NO LOCATION");
-                        }
 
-                        String[] userLists = holder.participants.toString().split(",");
-                        StringBuilder MailIds = new StringBuilder();
+                    insertFinalMeeting(scheduleArrayList, position, parent);
+                    pushNotification(position);
 
-                        for (int i = 0; i < userLists.length; i++) {
-                            MailIds.append("'" + userLists[i] + "'");
-
-                            if (i != userLists.length - 1) {
-                                MailIds.append(",");
-                            }
-                        }
-
-                        Log.d(TAG, "Sending Message To: " + MailIds.toString());
-                        Log.d(TAG, "Sending Message: " + message.toString());
-
-                        sendMessage(MailIds.toString(), message.toString());
-
-
-                    } catch (Exception e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
                     // Add the activity to where you want to redirect to after sending the notification.
                 }
             });
@@ -130,11 +105,15 @@ public class FreeTimeSchedulerInitiator  extends ArrayAdapter {
         {
             holder.txtTime.setText(scheduleArrayList.get(position).getTimeSlot()+"AM");
         }
+        StringBuilder partStringBuilder= partStringBuilder=new StringBuilder();
         for(int i=0;i<scheduleArrayList.get(position).getParticipantDetails().size();i++)
         {
-            holder.participants.setText(scheduleArrayList.get(position).getParticipantDetails().get(i).getParticipantName()+"\n");
-        }
 
+
+            partStringBuilder.append(scheduleArrayList.get(position).getParticipantDetails().get(i).getParticipantName() + "\n");
+
+        }
+        holder.participants.setText(partStringBuilder.toString());
         holder.txtMeetName.setText(meetingName);
         holder.txtLocation.setText(meetingLocation);
         holder.txtDate.setText(meetingDate);
@@ -168,7 +147,7 @@ public class FreeTimeSchedulerInitiator  extends ArrayAdapter {
                 AppConfig.URL_SENDMESS, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                Log.d(TAG, "Send Message Response: " + response);
+                //Log.d(TAG, "Send Message Response: " + response);
 
                 try {
                     JSONObject jObj = new JSONObject(response);
@@ -186,7 +165,7 @@ public class FreeTimeSchedulerInitiator  extends ArrayAdapter {
 
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, "Error Sending Message GCM: " + error.getMessage());
+                // Log.e(TAG, "Error Sending Message GCM: " + error.getMessage());
             }
         }) {
 
@@ -203,5 +182,148 @@ public class FreeTimeSchedulerInitiator  extends ArrayAdapter {
         };
         // Adding request to request queue
         AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+    private void insertFinalMeeting(List<TimeParticipantMap> participants,int position,ViewGroup parent) {
+        {
+
+            //Database
+
+            for(int i=0;i<scheduleArrayList.get(position).getParticipantDetails().size();i++)
+            {
+                users.append(scheduleArrayList.get(position).getParticipantDetails().get(i).getParticipantName() + ",");
+                createFinalMeet(meetingName, meetingDate, scheduleArrayList.get(position).getTimeSlot(), meetingLocation, scheduleArrayList.get(position).getMeetingid(), parent, scheduleArrayList.get(position).getParticipantDetails().get(i).getParticipantName());
+            }
+
+
+            Intent joinIntent= new Intent(parent.getContext(),MeetingCreated.class);
+
+           /* joinIntent.putExtra("meet_name", meetingName);
+            joinIntent.putExtra("meet_location",meetingLocation);
+            joinIntent.putExtra("meet_date",meetingDate);
+            joinIntent.putExtra("meet_time",scheduleArrayList.get(position).getTimeSlot());*/
+
+            parent.getContext().startActivity(joinIntent);
+            Toast.makeText(parent.getContext(), "Meeting is scheduled", Toast.LENGTH_LONG).show();
+            //pushNotification(position);
+
+        }
+    }
+    private void pushNotification(int position) {
+        try
+        {
+            StringBuilder  message = new StringBuilder();
+            message.append("FreeTimeScheduler;");
+            message.append(meetingName + ";");
+            message.append(meetingDate + ";");
+            message.append(scheduleArrayList.get(position).getTimeSlot() + ";");
+            message.append(scheduleArrayList.get(position).getMeetingid());
+
+            if (!meetingLocation.isEmpty()) {
+                message.append(meetingLocation);
+            } else {
+                message.append("NO LOCATION");
+            }
+
+            String[] userLists = users.toString().split(",");
+            StringBuilder MailIds = new StringBuilder();
+
+            for (int i = 0; i < userLists.length; i++) {
+                MailIds.append("'" + userLists[i] + "'");
+
+                if (i != userLists.length - 1) {
+                    MailIds.append(",");
+                }
+            }
+
+            //  Log.d(TAG, "Sending Message To: " + MailIds.toString());
+            // Log.d(TAG, "Sending Message: " + message.toString());
+            System.out.println("Mail Ids to send notification"+MailIds.toString());
+            sendMessage(MailIds.toString(), message.toString());
+
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+    private void createFinalMeet(final String meetingName, final String meetingDate, final int timeSlot, final String meetingLocation, final String meetingid, final ViewGroup parent, final String userLists) {
+
+        // Tag used to cancel the request
+        String tag_string_req = "req_createmeet";
+       /* pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+        pDialog.setMessage("Creating...");
+        showDialog();*/
+        pDialog.setMessage("Creating...");
+        showDialog();
+
+        StringRequest strReq = new StringRequest(Request.Method.POST,
+                AppConfig.URL_SCHEDULEFINALMEET, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //Log.d(TAG, "Create Meeting Response: " + response);
+                hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+                    if (!error) {
+
+
+
+
+                    } else {
+
+                        // Error occurred in registration. Get the error
+                        // message
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(parent.getContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //Log.e(TAG, "Error Creating Meeting: " + error.getMessage());
+                Toast.makeText(parent.getContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+        })  {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting params to create meeting url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("meetName", meetingName);
+                params.put("meetDate", meetingDate);
+                params.put("meetTimeFrom", timeSlot+"");
+                params.put("meetTimeTo", timeSlot+1+"");
+                params.put("meetLocation", meetingLocation);
+                params.put("meetId",meetingid);
+                params.put("Participants", userLists);
+
+                return params;
+            }
+
+        };
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
     }
 }
